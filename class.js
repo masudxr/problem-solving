1ebf5476427cf16bcda7246e30bed8e5050e1899
masudxr <masudxr@example.com>
2022-12-26 15:53:55 +0600

    class and method

---
+++ class.js
@@ -0,0 +1,24 @@
+class Car {
+   cname
+   model
+   height
+   color
+
+   setName(name){
+    this.cname = name;
+   }
+
+   setModel(model){
+    this.model = model
+   }
+}
+
+let Yama = new Car ();
+Yama.setName('Yamaha');
+Yama.setModel(2025);
+// console.log(Yama.cname);
+// console.log(Yama.model);
+console.log(Yama)
+
+
+
